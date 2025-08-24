db118ee90fb3153b4743ab532a4a6d2a58a6bb6b
Stanislas <no>
2025-08-24 15:26:09 +0200

    clean

--- app/src/main/java/com/example/pokedex/domain/repository/PokeAPIRepositoryImpl.kt
+++ app/src/main/java/com/example/pokedex/domain/repository/PokeAPIRepositoryImpl.kt
@@ -2,6 +2,4 @@ package com.example.pokedex.domain.repository
 
 import com.example.pokedex.data.repository.PokeAPIRepository
 
-class PokeAPIRepositoryImpl(val toto: Int, val titi: String): PokeAPIRepository {
-
-}
+class PokeAPIRepositoryImpl(): PokeAPIRepository {}
--- app/src/main/java/com/example/pokedex/module/AppModule.kt
+++ app/src/main/java/com/example/pokedex/module/AppModule.kt
@@ -2,38 +2,19 @@ package com.example.pokedex.module
 
 import com.example.pokedex.data.repository.PokeAPIRepository
 import com.example.pokedex.domain.repository.PokeAPIRepositoryImpl
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-/*@Module
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
 
     @Singleton
     @Provides
-    fun providePokeAPIRepository(toto: Int, titi: String): PokeAPIRepository {
-        return PokeAPIRepositoryImpl(toto = toto, titi = titi)
+    fun providePokeAPIRepository(): PokeAPIRepository {
+        return PokeAPIRepositoryImpl()
     }
-
-    @Provides
-    fun provideToto(): Int = 3
-
 }
-
-
-@Module
-abstract class Repo {
-
-    @Singleton
-    @Binds
-    abstract fun providePokeAPIRepository(monq: PokeAPIRepositoryImpl): PokeAPIRepository
-
-    @Provides
-    fun provideToto(): Int = 3
-}*/
-
-
-
-
